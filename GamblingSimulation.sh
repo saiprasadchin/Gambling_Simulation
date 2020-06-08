7994bb6a7c2bd50c097bf05aebcbd2d2310222b1
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-08 01:28:23 -0700

    GamblingUC3

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,14 +1,20 @@
 echo "Wellcome to Gambling Simulation"
 
-stake[1]=100
-won=0
-lost=0
-bet=$((RANDOM % 2))
-if [[ $bet -eq 1 ]]
-then
-echo "won "
-else
-echo "lost"
-fi
 
+stake=100
+upper=150
+lower=50
+
+while [[ $stake -gt $lower && $stake -lt $upper  ]]
+do
+        
+        bet=$(( RANDOM % 2 ))
+        if [[ $bet -eq 1 ]]
+        then
+                let "stake+=1"
+        else
+                let "stake-=1"
+        fi
+done
+echo "satke of  $ $stake"
 
